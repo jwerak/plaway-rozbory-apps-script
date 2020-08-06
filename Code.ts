b57663957eef99ae72d962878b30d2de9d213003
Jakub Veverka <jveverka@localhost.localdomain>
2020-08-06 20:39:06 +0200

    minor refactor and helper scripts

--- Code.ts
+++ Code.ts
@@ -1,16 +1,18 @@
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
   ui.createMenu('Generuj Reporty')
-    .addItem('Generuj Analyzy dohromady', 'GenerateAnalysesAll')
-    .addItem('Generuj Analyzy zvlast pro plavce', 'GenerateAnalysesPerSwimmer')
+    .addItem('Generuj Analyzy', 'GenerateAll')
     .addToUi();
 }
 
+function GenerateAll() {
+  const analyses = getAllAnalyses()
+  GenerateAnalysesAll(analyses)
+  GenerateAnalysesPerSwimmer(analyses)
+}
 
 // Generate analyses for all swimmers
-function GenerateAnalysesAll() {
-  const analyses = getAllAnalyses()
-
+function GenerateAnalysesAll(analyses) {
   let analysesJSON = {
     "name": "all",
     "reports": []
@@ -19,29 +21,18 @@ function GenerateAnalysesAll() {
   const names = analyses.forEach(
     row => {
       if (row[0] != "") {
-        analysesJSON.reports.push(
-        {
-          "recommendedDrills": row[8],
-          "toImprove": row[7],
-          "whatsGreat": row[6],
-          "souhra": row[5],
-          "legs": row[4],
-          "arms": row[3],
-          "style": row[2],
-          "name": row[1]}
-        )
+        analysesJSON.reports.push(getReportDict(row))
       }
     }
   )
 
-  downloadReports('https://plaway-docs.ey.r.appspot.com/analyses', analysesJSON)
+  const baseFolder = getFolder(getParentFolder(), 'rozbory')
+  downloadReports(baseFolder, 'https://plaway-docs.ey.r.appspot.com/analyses', analysesJSON)
 }
 
 
 // Generate per swimmer analyses
-function GenerateAnalysesPerSwimmer(){
-  const analyses = getAllAnalyses()
-
+function GenerateAnalysesPerSwimmer(analyses) {
   let analysesJSON = {}
 
   const names = analyses.forEach(
@@ -56,24 +47,30 @@ function GenerateAnalysesPerSwimmer(){
         }
 
         // Add analysis
-        analysesJSON[row[1]].reports.push(
-          {
-            "recommendedDrills": row[8],
-            "toImprove": row[7],
-            "whatsGreat": row[6],
-            "souhra": row[5],
-            "legs": row[4],
-            "arms": row[3],
-            "style": row[2],
-            "name": row[1]}
-        )
+        analysesJSON[row[1]].reports.push(getReportDict(row))
       }
     }
   )
+
+  const baseFolder = getFolder(getParentFolder(), 'rozbory')
   for (const key in analysesJSON) {
     const swimmerReport = analysesJSON[key];
-    downloadReports('https://plaway-docs.ey.r.appspot.com/analyses', swimmerReport)
+    const swimmerFolder = getFolder(baseFolder, swimmerReport.name)
+    downloadReports(swimmerFolder, 'https://plaway-docs.ey.r.appspot.com/analyses', swimmerReport)
   }
+}
+
+
+function getReportDict(row) {
+  return {
+    "recommendedDrills": row[8],
+    "toImprove": row[7],
+    "whatsGreat": row[6],
+    "souhra": row[5],
+    "legs": row[4],
+    "arms": row[3],
+    "style": row[2],
+    "name": row[1]}
 }
 
 // Return all analyses in report
@@ -84,7 +81,7 @@ function getAllAnalyses() {
     .slice(2);
 }
 
-function downloadReports(baseURL: string, data) {
+function downloadReports(destinationFolder: GoogleAppsScript.Drive.Folder, baseURL: string, data) {
   let fileName = "";
   let fileSize = 0;
   var options = {
@@ -100,12 +97,45 @@ function downloadReports(baseURL: string, data) {
 
   if (rc == 200) {
     const fileBlob = response.getBlob()
-    fileBlob.setName(data.name + ".pdf")
-    DriveApp.createFile(fileBlob)
+    createUniqueFileInFolder(destinationFolder, fileBlob, data.name + ".pdf")
   }
 }
 
 // Return empty folder relative to this sheet
-function getEmptyFolderForSwimmer(name: string) {
+function getFolder(baseFolder: GoogleAppsScript.Drive.Folder, name: string) {
+  const folders = baseFolder.getFolders()
+  while (folders.hasNext()) {
+    const folder = folders.next();
+    if (folder.getName() == name) {
+      Logger.log("Returning existing folder " + name + " in " + baseFolder.getName())
+      return folder
+    }
+  }
 
+  Logger.log("Creating folder " + name + " in " + baseFolder.getName())
+  return baseFolder.createFolder(name)
+}
+
+function createUniqueFileInFolder(folder: GoogleAppsScript.Drive.Folder, fileBlob: GoogleAppsScript.Base.Blob, filename: string) {
+  const folderName = folder.getName()
+
+  Logger.log("Creating file " + filename + " in folder " + folderName)
+  const files = folder.getFiles()
+  while (files.hasNext()) {
+    const file = files.next();
+    if (file.getName() == filename) {
+      Logger.log("Deleting existing file " + filename + " in folder" + folderName)
+      file.setTrashed(true)
+    }
+  }
+
+  fileBlob.setName(filename)
+  folder.createFile(fileBlob)
+}
+
+function getParentFolder() {
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const fileInDrive = DriveApp.getFolderById(ss.getId());
+
+  return fileInDrive.getParents().next()
 }
